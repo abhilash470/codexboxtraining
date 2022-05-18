15992aa9fbb1a38188c8c673ac02079f0f4f27a7
CodexBox <vidya.darnacodexbox.com>
2022-05-18 15:54:05 +0530

    modification in first application

--- CodexBoxJavaTrainingApplication/src/javatraining/basic/FirstApplication.java
+++ CodexBoxJavaTrainingApplication/src/javatraining/basic/FirstApplication.java
@@ -3,9 +3,17 @@ package javatraining.basic;
 public class FirstApplication {
     public static void main(String[] args){
         System.out.println("welcome to Codex box");
+        System.out.println("welcome to git hub");
         a1();
+        a2();
     }
+
+    private static void a2() {
+        System.out.println(234);
+    }
+
     static void a1(){
         System.out.println(123);
     }
+
 }
